21c769cc393661221d773ef157e4cce0b33cb6bc
Faur Ioan-Aurel <fioan89@gmail.com>
2016-02-15 22:35:34 +0200

    Started to work on the text style model. Will keep all metadata related to a pdf text line, like, centering, font, font size, color, char height and width.

--- src/com/fioan89/pdfchef/Style.java
+++ src/com/fioan89/pdfchef/Style.java
@@ -1,7 +1,32 @@
 package com.fioan89.pdfchef;
 
+import com.sun.istack.internal.NotNull;
+import com.sun.prism.paint.Color;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+
 /**
  * Created by fauri on 15/02/16.
  */
 public class Style {
+    private final PDFont font;
+    private final float fontSize;
+    private final Color color;
+    private final Centering centering;
+
+    public Style(@NotNull PDFont font, float fontSize) {
+        this(font, fontSize, Color.BLACK);
+    }
+
+    public Style(@NotNull PDFont font, float fontSize, Color textColor) {
+        this(font, fontSize, textColor, Centering.LEFT);
+    }
+
+    public Style(@NotNull PDFont font, float fontSize, Color textColor, Centering centering) {
+        this.font = font;
+        this.fontSize = fontSize;
+        this.color = textColor;
+        this.centering = centering;
+    }
+
+    public enum Centering {LEFT, RIGHT, CENTER}
 }
